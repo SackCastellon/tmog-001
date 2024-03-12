7a7ae4c5900c687ec06ad3af0c1971cdbe7f2e3b
Juan José González Abril <juanjo@gonzabril.es>
2024-03-12 08:48:00 +0100

    Implement solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,27 @@
 package org.tfoc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indices = new HashMap<>();
 
-    public int[] twoSum(
-            int[] nums,
-            int target) {
+        for (int index = 0; index < nums.length; index++) {
+            var num = nums[index];
+            var diff = target - num;
+            var diffIndex = indices.get(diff);
 
-        return null;
+            if (diffIndex == null) {
+                indices.put(num, index);
+            } else {
+                return new int[] {index, diffIndex};
+            }
+        }
+
+        throw new IllegalStateException("No solution found!");
     }
-
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,34 @@
 package org.tfoc;
 
-import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-@Slf4j
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    private Solution solution;
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
     }
 
+    @Test
+    void example1() {
+        var result = solution.twoSum(new int[] {2, 7, 11, 15}, 9);
+        assertThat(result).containsExactlyInAnyOrder(0, 1);
+    }
+
+    @Test
+    void example2() {
+        var result = solution.twoSum(new int[] {3, 2, 4}, 6);
+        assertThat(result).containsExactlyInAnyOrder(1, 2);
+    }
+
+    @Test
+    void example3() {
+        var result = solution.twoSum(new int[] {3, 3}, 6);
+        assertThat(result).containsExactlyInAnyOrder(0, 1);
+    }
 }
